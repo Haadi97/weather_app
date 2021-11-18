1f769a20965487c775c0ad3925c22911daeaab02
Haadi <haadi@synomus.io>
2021-11-18 17:16:40 +0800

    add comments

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   // this is a comment
   // CONFLICT
   // ANOTHER CONFLICT
+  // here is a comment
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
